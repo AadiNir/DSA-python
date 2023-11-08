d55b688522b30afc7502d023f53f3b0e4bf5c2de
Aadinir <aadinir@Aadinir.myguest.virtualbox.org>
2023-11-08 09:07:30 +0530

    creating node

---
+++ linkedlist.java
@@ -0,0 +1,14 @@
+class linkedlist{
+    class Node{
+        String data;
+        Node next;
+        Node(String data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public class static void main(String[] args){
+
+    }
+}
